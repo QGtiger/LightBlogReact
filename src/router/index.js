44596da127f37b409780bf24a7222eaedde1feb3
QGtiger <1426286337@qq.com>
2020-02-04 20:52:36 +0800

    antd

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link, Route, HashRouter as Router, Switch } from 'react-router-dom';
 import App from '../App';
-import LoginView from '../views/login/login'
+import LoginView from '../views/login/login';
+import ButtonView from '../views/button/button'
 
 export default ()=>(
     <Router>
         <Switch>
             <Route exact path="/" component={App}></Route>
             <Route exact path="/login" component={LoginView}></Route>
+            <Route exact path="/button" component={ButtonView}></Route>
         </Switch>
     </Router>
 )
